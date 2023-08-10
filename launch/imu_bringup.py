e4f5d46fac16e2e6ede3549c7712aa75a6e44a95
yuki yoshida <yoshida-yuuki@cuborex.com>
2023-08-10 10:40:36 +0900

    不要なimportを削除

--- launch/imu_bringup.py
+++ launch/imu_bringup.py
@@ -1,16 +1,9 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import PathJoinSubstitution, TextSubstitution
-
 import os
-from pathlib import Path
 
 from ament_index_python.packages import get_package_share_directory
-
-from launch.actions import LogInfo
-from launch.substitutions import LaunchConfiguration
 	
 def generate_launch_description():
     return LaunchDescription([        
