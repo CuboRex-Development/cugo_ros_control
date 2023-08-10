071aa987fcdb77511edfe7acddf3eeb4e7770249
yuki yoshida <yoshida-yuuki@cuborex.com>
2023-08-10 10:19:33 +0900

    不要なコードを削除

--- launch/lidar_bringup.py
+++ launch/lidar_bringup.py
@@ -2,7 +2,6 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import PathJoinSubstitution, TextSubstitution
 
 import os
 from pathlib import Path
@@ -12,18 +11,8 @@ from ament_index_python.packages import get_package_share_directory
 from launch.actions import LogInfo
 from launch.substitutions import LaunchConfiguration
 
-# import math
 	
 def generate_launch_description():
-    # conf file path
-    conf_path = os.path.dirname(os.path.abspath(__file__))
-    conf_path = conf_path[:-57]
-
-    # args for laserfilter
-    laser_filter_file = DeclareLaunchArgument(
-        'laser_filter_file', default_value='laser_filters/v3ros_filter.yaml'
-    )
-    
     # args for lidar
     channel_type     = LaunchConfiguration('channel_type', default='udp')
     udp_ip           = LaunchConfiguration('udp_ip', default='192.168.11.2')
@@ -34,6 +23,11 @@ def generate_launch_description():
     scan_mode        = LaunchConfiguration('scan_mode', default='Sensitivity')
     scan_frequency   = LaunchConfiguration('scan_frequency', default='10')
 
+    # args for laserfilter
+    laser_filter_file = DeclareLaunchArgument(
+        'laser_filter_file', default_value='laser_filters/v3ros_filter.yaml'
+    )
+
     return LaunchDescription([
         # static TF
         Node(
@@ -42,7 +36,7 @@ def generate_launch_description():
             arguments = ['0','0','0.16','0','0','3.14','base_link','laser']
         ),
         
-        # LiDAR
+        # LiDAR arg
         DeclareLaunchArgument(
             'channel_type',
             default_value=channel_type,
@@ -79,6 +73,7 @@ def generate_launch_description():
             description='Specifying scan mode of lidar'
         ),
 
+        # LiDAR
         Node(
             package='rplidar_ros',
             executable='rplidar_node',
@@ -100,22 +95,12 @@ def generate_launch_description():
         Node(
             package = 'laser_filters', 
             executable = 'scan_to_scan_filter_chain',
-            # name = 'laser_filters',
             parameters=[
-
                 os.path.join(get_package_share_directory('cugo_ros2_control') , 'config/laser_filters/v3ros_filter.yaml')
             ],
-
             remappings = [
                 ('scan','scan_raw'),
                 ('scan_filtered','scan')
             ]
-        ),
-
-        # rviz2
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2'
         )
     ])
